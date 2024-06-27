7a6c577f89e8d8b3f27e2be83716a914e9bcba11
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-06-27 14:03:03 -0400

    Begin file to test integration between classes

---
+++ tests/test_particleoperator_fock_relations.py
@@ -0,0 +1,10 @@
+from openparticle import (
+    ParticleOperator,
+    ParticleOperatorSum,
+    Fock,
+    FockSum,
+    ConjugateFock,
+    ConjugateFockSum,
+)
+import numpy as np
+import pytest
