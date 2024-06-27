cd6bf224bbc059a53374b76d7eabf1d8e16ade43
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-06-27 11:29:14 -0400

    Summing fock state tests

--- tests/test_fockstate.py
+++ tests/test_fockstate.py
@@ -31,15 +31,61 @@ def test_fock_state_mul_by_constant(coeff):
 
 def test_fock_states_add_two_different_states():
     state_1 = Fock([1], [2], [(0, 4)])
-
     state_2 = Fock([0], [], [(1, 1)])
 
     state_sum = state_1 + state_2
-
-    # assert state_1 + state_2 == state_sum
     assert state_1 + state_2 == FockSum([state_1, state_2])
 
 
 def test_fock_states_add_two_same_states():
     state = Fock([1], [2], [(0, 4)])
     assert state + state == 2 * state
+
+
+def test_add_fock_to_focksum():
+    fsum = Fock([1], [2], []) + Fock([0], [], [])
+    state = Fock([1], [], [])
+
+    assert fsum + state == FockSum([fsum.states_list[0], fsum.states_list[1], state])
+
+
+def test_add_focksum_to_fock_with_proper_ordering():
+    fsum = Fock([1], [2], []) + Fock([0], [], [])
+    state = Fock([1], [], [])
+
+    assert state + fsum == FockSum([state, fsum.states_list[0], fsum.states_list[1]])
+
+
+def test_add_focksum_to_fock_with_wrong_ordering():
+    fsum = Fock([1], [2], []) + Fock([0], [], [])
+    state = Fock([1], [], [])
+
+    assert state + fsum == FockSum([fsum.states_list[0], fsum.states_list[1], state])
+
+
+def test_add_focksum_to_different_focksum():
+    fsum1 = Fock([1], [2], []) + Fock([0], [], [])
+    fsum2 = Fock([3], [2], []) + Fock([1], [], [])
+
+    assert fsum1 + fsum2 == FockSum(
+        [
+            fsum1.states_list[0],
+            fsum1.states_list[1],
+            fsum2.states_list[0],
+            fsum2.states_list[1],
+        ]
+    )
+
+
+def test_add_focksum_to_overlapping_focksum():
+    fsum1 = Fock([1], [2], []) + Fock([0], [], [])
+    fsum2 = Fock([3], [2], []) + Fock([0], [], [])
+
+    assert fsum1 + fsum2 == Fock([1], [2], []) + 2 * Fock([0], [], []) + Fock(
+        [3], [2], []
+    )
+
+
+def test_focksum_normlization():
+    fsum = Fock([1], [2], []) + Fock([0], [], [])
+    assert fsum.normalize() == 1 / np.sqrt(2) * fsum
