a0c20bc56868c60b51a58820ca8ebdf42f2791dd
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-06-28 15:17:45 -0400

    Add A|f> for defined states and simple matrix element

--- tests/test_class_integration.py
+++ tests/test_class_integration.py
@@ -8,3 +8,22 @@ from openparticle import (
 )
 import numpy as np
 import pytest
+
+
+def test_defined_matrix_element():
+    bra = ConjugateFock([1], [], [])
+    operator = ParticleOperator("b1^")
+    ket = Fock([], [], [])
+
+    assert bra * operator * ket == 1.0
+
+
+def test_defined_particleoperator_acts_on_fock_state():
+    operator = ParticleOperator("b0^")
+    state = Fock([], [1], [])
+
+    assert operator * state == Fock([0], [1], [])
+
+
+def test_random_particleoperator_acts_on_fock_state():
+    pass
