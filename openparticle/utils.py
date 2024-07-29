9819d4967695ec7a148c1aba800236292e016afd
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-07-29 10:31:18 -0400

    Fix more imports due to failed tests

--- openparticle/utils.py
+++ openparticle/utils.py
@@ -1,16 +1,16 @@
-from openparticle import Fock
+from openparticle import Fock, ParticleOperator
 import numpy as np
 from typing import List
 
 
 def get_computational_states_fermionic(n_modes):
     return [
-        op.Fock([int(j) for j in format(i, "0" + str(n_modes) + "b")][::-1], [], [])
+        Fock([int(j) for j in format(i, "0" + str(n_modes) + "b")][::-1], [], [])
         for i in range(2**n_modes)
     ]
 
 
-def generate_matrix_from_basis(operator: op.ParticleOperator, basis: List[Fock]):
+def generate_matrix_from_basis(operator: ParticleOperator, basis: List[Fock]):
     size = (len(basis), len(basis))
     matrix = np.zeros(size)
 
