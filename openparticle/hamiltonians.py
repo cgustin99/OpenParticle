add1c70f2772c05afadbb9a75fdf2c25a715ad60
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-10-02 10:32:20 -0400

    Rescale Hamiltonian

--- openparticle/hamiltonians.py
+++ openparticle/hamiltonians.py
@@ -5,7 +5,7 @@ from openparticle import ParticleOperator
 
 
 # Non-interacting Hamiltonians
-def free_boson_Hamiltonian(Lambda, m_B=1, L=1):
+def free_boson_Hamiltonian(Lambda, m_B=1, L=2 * np.pi):
     free_scalar = ParticleOperator({})
     for k in [i for i in range(-Lambda, Lambda + 1) if i != 0]:
         free_scalar += (
@@ -13,15 +13,15 @@ def free_boson_Hamiltonian(Lambda, m_B=1, L=1):
         ).normal_order()
     free_scalar.remove_identity()
 
-    return L * m_B**2 / (2 * np.pi) * free_scalar
+    return 2 * np.pi * m_B**2 / (4 * L**2) * free_scalar
 
 
 def free_fermion_Hamiltonian(Lambda, m_F=1, L=1):
     free_fermi = np.array([ParticleOperator({})]).reshape([1, -1])
-    for k in np.arange(-Lambda + 1 / 2, Lambda):
+    for k in np.arange(-Lambda + 1 / 2, Lambda + 1 / 2):
         free_fermi += (
-            m_F**2
-            / (2 * L * p(k))
+            1
+            / (p(k, L=L))
             * (
                 FermionField(k, m_F, L).psi_dagger.dot(
                     Lambdap.dot(FermionField(k, m_F, L).psi)
@@ -31,34 +31,36 @@ def free_fermion_Hamiltonian(Lambda, m_F=1, L=1):
 
     free_fermi[0][0].remove_identity()
 
-    return L * m_F**2 / (2 * np.pi) * free_fermi[0][0]
+    return 2 * m_F**2 / (4 * L**2) * free_fermi[0][0]
 
 
 # Yukawa Interaction
 def three_point_yukawa(Lambda, g=1, L=1):
     three_point = ParticleOperator({})
-    for k1 in np.arange(-Lambda + 1 / 2, Lambda):
-        for k2 in np.arange(-Lambda + 1 / 2, Lambda):
+    for k1 in np.arange(-Lambda + 1 / 2, Lambda + 1 / 2):
+        for k2 in np.arange(-Lambda + 1 / 2, Lambda + 1 / 2):
             for k3 in [i for i in range(-Lambda, Lambda + 1) if i != 0]:
                 if k1 == k2 + k3:
-                    three_point += (
+                    op = (
                         FermionField(k1).psi_dagger.dot(
                             gamma0.dot(FermionField(k2).psi)
                         )[0][0]
                         * ScalarField(k3).phi
-                    ).normal_order()
+                    )
+                    if len(op.op_dict) != 0:
+                        three_point += op.normal_order()
 
     three_point.remove_identity()
-    return g / (2 * L) ** 2 * three_point
+    return 4 * np.pi * g / (2 * L) ** 3 * three_point
 
 
 def instantaneous_yukawa(Lambda, g=1, L=1):
     four_point = ParticleOperator({})
 
-    for k1 in np.arange(-Lambda + 1 / 2, Lambda):
+    for k1 in np.arange(-Lambda + 1 / 2, Lambda + 1 / 2, 1):
         for k2 in [i for i in range(-Lambda, Lambda + 1) if i != 0]:
             for k3 in [i for i in range(-Lambda, Lambda + 1) if i != 0]:
-                for k4 in np.arange(-Lambda + 1 / 2, Lambda, 1):
+                for k4 in np.arange(-Lambda + 1 / 2, Lambda + 1 / 2, 1):
                     if k1 == k2 + k3 + k4:
                         four_point += (
                             1
@@ -73,7 +75,7 @@ def instantaneous_yukawa(Lambda, g=1, L=1):
                         )[0][0].normal_order()
     four_point.remove_identity()
 
-    return g**2 / (2 * L) ** 3 * four_point
+    return 4 * np.pi * g**2 / (2 * L) ** 4 * four_point
 
 
 def yukawa_Hamiltonian(Lambda, g=1, L=1, m_B=1, m_F=1):
