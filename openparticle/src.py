Fix bosonic normalization

openparticle/src.py
@@ -7,6 +7,7 @@ import re
 from copy import deepcopy
 from itertools import product
 import math
+import scipy
 
 
 class ParticleOperator:
@@ -763,12 +764,14 @@ class ParticleOperator:
                 af_parity = 1
             if 2 in split_key:
                 b_occ = [(num, count) for num, count in Counter(split_key[2]).items()]
-                b_coeff = len(split_key[2])
+                b_coeff = np.prod(
+                    np.sqrt(scipy.special.factorial(np.array(b_occ)[:, 1]))
+                )
             else:
                 b_occ = []
                 b_coeff = 1
 
-            coeff = val * f_parity * af_parity * np.sqrt(math.factorial(b_coeff))
+            coeff = val * f_parity * af_parity * b_coeff
             state_dict_key = (tuple(f_occ), tuple(af_occ), tuple(b_occ))
             state_dict[state_dict_key] = coeff + state_dict.get(state_dict_key, 0)
 
@@ -838,10 +841,15 @@ class Fock(ParticleOperator):
             af_tup = tuple([(1, i, 1) for i in af_occ])
             b_tup = tuple((2, i, 1) for i, b in b_occ for _ in range(b))
 
+            if b_occ != []:
+                b_coeff = np.prod(
+                    np.sqrt(scipy.special.factorial(np.array(b_occ)[:, 1]))
+                )
+            else:
+                b_coeff = 1
+
             key = f_tup + af_tup + b_tup
-            self.state_opdict = {
-                key: coeff / np.sqrt(max(math.factorial(len(b_tup)), 1))
-            }
+            self.state_opdict = {key: coeff / b_coeff}
 
             super().__init__(self.state_opdict, coeff=coeff)
 

tests/test_fockstate.py
@@ -1,6 +1,7 @@
-from openparticle import Fock
+from openparticle import Fock, ParticleOperator
 import pytest
 import numpy as np
+import math
 
 
 def test_fock_state_stores_correct_occupancies():
@@ -29,3 +30,25 @@ def test_fock_state_mul_by_constant(coeff):
 def test_fock_states_add_two_different_states():
     state = Fock([1, 2], [], []) + Fock([], [1], [])
     assert state.op_dict == {((0, 1, 1), (0, 2, 1)): 1.0, ((1, 1, 1),): 1.0}
+
+
+def test_bosonic_fock_state_generation():
+    state = Fock([], [], [(0, 3)])
+    op = 1 / (np.sqrt(math.factorial(3))) * ParticleOperator("a0^ a0^ a0^")
+    vacuum = Fock([], [], [])
+
+    assert (op * vacuum).coeff == state.coeff
+
+
+def test_bosonic_fock_state_generation_2():
+    vacuum = Fock([], [], [])
+    op = ParticleOperator("a0^ a1^")
+    expected_state = Fock([], [], [[0, 1], [1, 1]])
+    assert (op * vacuum).coeff == expected_state.coeff  # == 1
+
+
+def test_bosonic_fock_state_generation_3():
+    vacuum = Fock([], [], [])
+    op = 1 / np.sqrt(2) * ParticleOperator("a0^ a0^ a1^")
+    expected_state = Fock([], [], [[0, 2], [1, 1]])
+    assert (op * vacuum).coeff == expected_state.coeff  # np.sqrt(2)