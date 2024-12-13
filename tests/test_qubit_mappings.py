06b27082990d9371b2ada3f61bafa27c3d723d60
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-12-13 11:37:24 -0500

    add to_paulis/to_qubit_state methods

--- tests/test_qubit_mappings.py
+++ tests/test_qubit_mappings.py
@@ -6,19 +6,19 @@ from symmer import PauliwordOp, QuantumState
 def test_JW_dagger():
     op = ParticleOperator("b0^")
     jw_op = PauliwordOp.from_dictionary({"X": 1 / 2, "Y": -1j / 2})
-    assert jordan_wigner(op) == jw_op
+    assert op.to_paulis() == jw_op
 
 
 def test_JW():
     op = ParticleOperator("b0")
     jw_op = PauliwordOp.from_dictionary({"X": 1 / 2, "Y": 1j / 2})
-    assert jordan_wigner(op) == jw_op
+    assert op.to_paulis() == jw_op
 
 
 def test_product_JW():
     op = ParticleOperator("b0^ b0")
     jw_op = PauliwordOp.from_dictionary({"I": 1 / 2, "Z": -1 / 2})
-    assert jordan_wigner(op) == jw_op
+    assert op.to_paulis() == jw_op
 
 
 def test_product_different_modes_JW():
@@ -26,7 +26,7 @@ def test_product_different_modes_JW():
     jw_op = PauliwordOp.from_dictionary(
         {"XX": 1 / 4, "XY": -1j / 4, "YX": 1j / 4, "YY": 1 / 4}
     )
-    assert jordan_wigner(op) == jw_op
+    assert op.to_paulis() == jw_op
 
 
 def test_spread_JW():
@@ -34,13 +34,13 @@ def test_spread_JW():
     jw_op = PauliwordOp.from_dictionary(
         {"XZZY": 0.25j, "YZZY": (0.25 + 0j), "XZZX": (0.25 + 0j), "YZZX": -0.25j}
     )
-    assert jordan_wigner(op) == jw_op
+    assert op.to_paulis() == jw_op
 
 
 def test_SB():
     op = ParticleOperator("a0")
     SB_op = PauliwordOp.from_dictionary({"X": 1 / 2, "Y": 1j / 2})
-    assert SB(op, 1) == SB_op
+    assert op.to_paulis(max_bose_occ=1) == SB_op
 
 
 def test_SB_higher_max_occ():
@@ -57,7 +57,7 @@ def test_SB_higher_max_occ():
             "YY": (0.3535533905932738 + 0j),
         }
     )
-    assert SB(op, 3) == SB_op
+    assert op.to_paulis(max_bose_occ=3) == SB_op
 
 
 def test_mapping_product_of_different_types_ops():
@@ -66,7 +66,7 @@ def test_mapping_product_of_different_types_ops():
     qubit_op = PauliwordOp.from_dictionary(
         {"XZX": (0.25 + 0j), "YZX": -0.25j, "XZY": 0.25j, "YZY": (0.25 + 0j)}
     )
-    assert op_qubit_map(op, max_bose_occ=max_bose_occ) == qubit_op
+    assert op.to_paulis(max_bose_occ=max_bose_occ) == qubit_op
 
 
 def test_fock_qubit_map():
@@ -77,14 +77,13 @@ def test_fock_qubit_map():
     max_f_mode = 5
     max_af_mode = 0
     max_b_mode = 2
-    max_occ = 3
+    max_bose_occ = 3
 
     mapped_bitstring = "010110" + "1" + "01" + "00" + "11"
     expected_quantum_state = QuantumState.from_dictionary({mapped_bitstring: 1.0})
     state = Fock(f_occ, af_occ, b_occ)
-    obtained_quantum_state = fock_qubit_state_mapping(
-        state=state,
-        max_occ=max_occ,
+    obtained_quantum_state = state.to_qubit_state(
+        max_bose_occ=max_bose_occ,
         max_fermion_mode=max_f_mode,
         max_antifermion_mode=max_af_mode,
         max_boson_mode=max_b_mode,
