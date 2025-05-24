4236b162b1716a5c81dfc592c0ff4bc2ab38f4d0
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2025-05-24 12:36:21 -0400

    Old plots I forgot to commit

--- openparticle/hamiltonians/yukawa_hamiltonians.py
+++ openparticle/hamiltonians/yukawa_hamiltonians.py
@@ -11,6 +11,34 @@ from itertools import product
 import time
 
 
+def index_to_k(operator):
+    # Returns momentum variable k for given operator and index
+    # e.g. b_0 returns k = 1/2, a2 returns k = 3
+    if operator.has_fermions or operator.has_antifermions:
+        return operator.modes[0][0] + 1 / 2
+    elif operator.has_bosons:
+        return operator.modes[0][0] + 1
+
+
+def resolution_constraint(terms, resolution):
+    # Remove terms with sum of all k created or annihilated > K
+    good_terms = {}
+
+    for term in terms:
+        k_created, k_annihilated = 0, 0
+        for op in term.split():
+            k = index_to_k(op)
+            if op.creation:
+                k_created += k
+            else:
+                k_annihilated += k
+        if k_created <= resolution and k_annihilated <= resolution:
+            key, val = list(term.op_dict.keys())[0], list(term.op_dict.values())[0]
+            good_terms[key] = val
+
+    return ParticleOperator(good_terms)
+
+
 # Yukawa Interaction
 def three_point_yukawa(res, g, mf, mb):
 
@@ -35,6 +63,7 @@ def three_point_yukawa(res, g, mf, mb):
                     container_dict[op_str] = coeff + container_dict.get(op_str, 0.0)
     three_point = ParticleOperator(container_dict)
     three_point.remove_identity()
+    three_point = resolution_constraint(three_point, res)
     return 2 * L * g / (2 * L) ** 3 * three_point  # * (P+ = 1)
 
 
@@ -63,8 +92,51 @@ def instantaneous_yukawa(res, g, mf, mb):
 
     four_point.remove_identity()
     four_point = remove_symmetry_terms(four_point, 4)
+    four_point = resolution_constraint(four_point, res)
 
     return 2 * L * g**2 / (2 * L) ** 4 * four_point  # * (P+ = 1)
+
+
+def instantaneous_yukawa_dummy(res):
+
+    fermion_lim = res - ((res - 0.5) % 1)
+    boson_lim = int(res)
+    fermionic_range = np.arange(-fermion_lim, fermion_lim + 1, 1)
+    bosonic_range = np.array([i for i in range(-boson_lim, boson_lim + 1) if i != 0])
+
+    L = 2 * np.pi * res
+    container_dict = dict()
+
+    for k1, k2, k3, k4 in product(
+        fermionic_range, bosonic_range, bosonic_range, fermionic_range
+    ):
+        if k1 == k2 + k3 + k4:
+            op1 = (k1 > 0) * ParticleOperator("b" + str(abs(int(k1 - 0.5))) + "^") + (
+                k1 < 0
+            ) * ParticleOperator("d" + str(abs(int(k1 - 0.5))))
+            op2 = (k2 > 0) * ParticleOperator("a" + str(abs(int(k2 - 1)))) + (
+                k2 < 0
+            ) * ParticleOperator("a" + str(abs(int(k2 + 1))) + "^")
+            op3 = (k3 > 0) * ParticleOperator("a" + str(abs(int(k3 - 1)))) + (
+                k3 < 0
+            ) * ParticleOperator("a" + str(abs(int(k3 + 1))) + "^")
+            op4 = (k4 > 0) * ParticleOperator("b" + str(abs(int(k4 - 0.5)))) + (
+                k4 < 0
+            ) * ParticleOperator("d" + str(abs(int(k4 - 0.5))) + "^")
+            op = (op1 * op2 * op3 * op4).normal_order().order_indices()
+            op = remove_symmetry_terms(op, 4)
+            op = (1 / op.coeff) * op
+            if len(op.op_dict) != 0:
+                helper_variable = op
+                for op_str, coeff in helper_variable.op_dict.items():
+                    container_dict[op_str] = coeff + container_dict.get(op_str, 0.0)
+    four_point = ParticleOperator(container_dict)
+
+    four_point.remove_identity()
+    four_point = remove_symmetry_terms(four_point, 4)
+    four_point = resolution_constraint(four_point, res)
+
+    return four_point
 
 
 def yukawa_hamiltonian(res, g, mf, mb):
