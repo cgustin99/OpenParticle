78217a76db1688694088839e44234e17340d9d74
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2025-06-26 16:26:17 -0400

    working map from tensor to particleoperator dictionary

--- models/qcd/hamiltonian/free_hamiltonian.py
+++ models/qcd/hamiltonian/free_hamiltonian.py
@@ -16,7 +16,7 @@ Nc = 3
     fastmath=True,
 )
 def free_quark_hamiltonian_tensor(K: float, Kp: float, mq: float):
-    quark_helicities = [1, -1]
+    quark_helicities = np.array([1, -1], dtype=np.int8)
     quark_colors = np.arange(1, Nc + 1, 1)
     fermion_lim = K - ((K - 0.5) % 1)
     fermion_longitudinal_q = np.array(
@@ -26,11 +26,11 @@ def free_quark_hamiltonian_tensor(K: float, Kp: float, mq: float):
     transverse_q = np.array([i for i in np.arange(-Kp, Kp + 1.0)], dtype=np.float64)
 
     dim = (
-        len(quark_colors),
-        len(quark_helicities),
         len(fermion_longitudinal_q),
         len(transverse_q),
         len(transverse_q),
+        len(quark_colors),
+        len(quark_helicities),
     )
     free_quark_tensor = np.zeros(dim, dtype=np.complex128)
 
@@ -43,16 +43,17 @@ def free_quark_hamiltonian_tensor(K: float, Kp: float, mq: float):
                         coeff = (mq**2 + q[1:3].dot(q[1:3])) / q[0]
 
                         color_index = color - 1
-                        helicity_index = helicity - 1
+                        helicity_index = np.where(helicity == quark_helicities)[0][0]
                         qplus_index = np.where(fermion_longitudinal_q == qplus)[0][0]
                         qperp1_index = np.where(transverse_q == qperp1)[0][0]
                         qperp2_index = np.where(transverse_q == qperp2)[0][0]
+
                         free_quark_tensor[
-                            color_index,
-                            helicity_index,
                             qplus_index,
                             qperp1_index,
                             qperp2_index,
+                            color_index,
+                            helicity_index,
                         ] = coeff
 
     return free_quark_tensor
@@ -74,11 +75,11 @@ def free_gluon_hamiltonian_tensor(K: float, Kp: float, mg: float):
     transverse_q = np.array([i for i in np.arange(-Kp, Kp + 1.0)], dtype=np.float64)
 
     dim = (
-        len(gluon_colors),
-        len(gluon_polarizations),
         len(boson_longitudinal_q),
         len(transverse_q),
         len(transverse_q),
+        len(gluon_colors),
+        len(gluon_polarizations),
     )
     free_gluon_tensor = np.zeros(dim, dtype=np.complex128)
 
@@ -97,11 +98,11 @@ def free_gluon_hamiltonian_tensor(K: float, Kp: float, mg: float):
                         qperp2_index = np.where(transverse_q == qperp2)[0][0]
 
                         free_gluon_tensor[
-                            color_index,
-                            polarization_index,
                             qplus_index,
                             qperp1_index,
                             qperp2_index,
+                            color_index,
+                            polarization_index,
                         ] = coeff
 
     return free_gluon_tensor
